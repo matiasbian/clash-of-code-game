50857fb0f49d461d2e6ad0cbf03cd77968440889
matiasbian <matiasezequielbian@gmail.com>
2023-08-24 18:11:32 -0300

    Move from point A to B method

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,11 +1,33 @@
-class_name Character extends Node
+extends CharacterBody2D
 
-var miclase = MiClase.new()
-# Called when the node enters the scene tree for the first time.
+@export var speed = 400
+
+#components
+var anim
+
+var target = Vector2(500,0)
+
 func _ready():
-	pass # Replace with function body.
+	anim = get_node("AnimationPlayer")
+	
+func _input(event):
+	if event.is_action_pressed("clicked"):
+		print("clicked")
+		movePlayerToPos(get_global_mouse_position())
+		
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	_handlePlayerMovement()
+		
+func _handlePlayerMovement():
+	if position.distance_to(target) > 5:
+		move_and_slide()
+	else:
+		anim.stop()
+		
+func movePlayerToPos(pos):
+	anim.play("Moving")
+	target = pos
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	miclase._print_algo()
