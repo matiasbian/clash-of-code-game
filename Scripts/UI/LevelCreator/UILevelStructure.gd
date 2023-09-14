0387c8968e51f4009f1523eed59266a975c85aa2
matiasbian <matiasezequielbian@gmail.com>
2023-09-13 21:17:11 -0300

    Level creation complete flow

--- Scripts/UI/LevelCreator/UILevelStructure.gd
+++ Scripts/UI/LevelCreator/UILevelStructure.gd
@@ -8,7 +8,14 @@ class_name UILevelStructure extends Node
 
 @export var add_button:Button = Button.new()
 @export var submit_button:Button = Button.new()
+
+@export var success_popup:Panel = Panel.new()
+@export var failure_popup:Panel = Panel.new()
+
 @export var http_req:HTTP_REQUESTS = HTTP_REQUESTS.new()
+
+@export var ok_color:Color = Color.WHITE
+@export var error_color:Color = Color.FIREBRICK
 
 @onready var dic:Dictionary
 var itemSelectedIndex:int
@@ -27,6 +34,12 @@ func _ready():
 	submit_button.pressed.connect(_submit)
 	levelNumber.value_changed.connect(_level_changed)
 	min_steps.value_changed.connect(_min_steps_changed)
+	
+	http_req.data_sent.connect(_open_success_popup)	
+	
+	success_popup.get_node("Container/Button").pressed.connect(_close_s)
+	failure_popup.get_node("Container/Button").pressed.connect(_close_f)
+	
 	_fill_dic()
 
 func _delete_item(index):
@@ -48,41 +61,30 @@ func _item_added():
 	print(steps)
 	
 func _submit():
-	var structure = _level_structure()
-	var error = false
-	#check label
-	if structure["label"] == "":
-		print("label is empty")
-		error = true
-	#check lvl number
-	if structure["levelNumber"] == 0:
-		print("level number cannot be 0")
-		error = true
-	#check steps amount
-	if structure["perfect_steps"] == 0:
-		print("perfect steps cannot be 0")
-		error = true
-
-	if (steps.size() > 0):
-		# check structure
-		var start_step:Dictionary = steps[0]
-		var end_step:Dictionary = steps[steps.size() -1]
-		
-		
-		if !(start_step.keys()[0] == dic[7].keys()[0] && end_step.keys()[0] == dic[8].keys()[0]):
-			error = true
-	else:
-		error = true
+	var structure = _level_structure()	
+	var error = _check_errors(structure)
 	
 	if (!error):
 		print("sending data")
 		http_req.HTTPPost(URL, structure)
-	
+	else:
+		failure_popup.visible = true
+
+func _open_success_popup(data):
+	success_popup.visible = true
+
 func _level_changed(value):
 	level_number_value = value
 
 func _min_steps_changed(value):
 	min_steps_value = value
+	
+func _close_s():
+	success_popup.visible = false
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	
+func _close_f():
+	failure_popup.visible = false	
 # aux
 
 func _level_structure():
@@ -129,4 +131,47 @@ func _fill_dic():
 			"dir": "Forward"
 		}
 	}
+
+func _check_errors(structure):
+	var error = false
+	#check label
+	if structure["label"] == "":
+		print("label is empty")
+		level_label.get_parent().get_node("Label").add_theme_color_override("font_color", error_color)
+		error = true
+	else:
+		level_label.get_parent().get_node("Label").add_theme_color_override("font_color", ok_color)
+		
+	#check lvl number
+	if structure["levelNumber"] == 0:
+		print("level number cannot be 0")
+		levelNumber.get_parent().get_node("Label").add_theme_color_override("font_color", error_color)
+		error = true
+	else:
+		levelNumber.get_parent().get_node("Label").add_theme_color_override("font_color", ok_color)
+	#check steps amount
+	if structure["perfect_steps"] == 0:
+		print("perfect steps cannot be 0")
+		min_steps.get_parent().get_node("Label").add_theme_color_override("font_color", error_color)
+		error = true
+	else:
+		min_steps.get_parent().get_node("Label").add_theme_color_override("font_color", ok_color)
+
+	if (steps.size() > 0):
+		# check structure
+		var start_step:Dictionary = steps[0]
+		var end_step:Dictionary = steps[steps.size() -1]
+		
+		
+		if !(start_step.keys()[0] == dic[7].keys()[0] && end_step.keys()[0] == dic[8].keys()[0]):
+			print("Structure format error")
+			error = true
+			itemList.get_parent().get_parent().get_node("Label").add_theme_color_override("font_color", error_color)
+		else:
+			itemList.get_parent().get_parent().get_node("Label").add_theme_color_override("font_color", ok_color)			
+	else:
+		print("Structure error: Empty")
+		itemList.get_parent().get_parent().get_node("Label").add_theme_color_override("font_color", error_color)		
+		error = true
+	return error
 	
--- Scripts/UI/MainMenu/PlayButton.gd
+++ Scripts/UI/MainMenu/PlayButton.gd
@@ -3,8 +3,14 @@ extends Button
 @onready var MainMenu = get_node("/root/MainMenu/UI/MainMenu")
 @onready var LevelsMenu = get_node("/root/MainMenu/UI/Levels")
 
+@export var levelEditButton:Button = Button.new()
 func _pressed():
 	MainMenu.visible = false
 	LevelsMenu.visible = true
+func _ready():
+	levelEditButton.pressed.connect(_edit_level_pressed)
+	
+func _edit_level_pressed():
+		get_tree().change_scene_to_file("res://Scenes/LevelEditor.tscn")
 
 
