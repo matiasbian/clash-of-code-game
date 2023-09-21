f8539186bfad05d8f548bb9e018ce1c54eaa6871
matiasbian <matiasezequielbian@gmail.com>
2023-09-21 19:46:32 -0300

    add level number to level thumbnail

--- Scripts/UI/MainMenu/LevelThumbnail.gd
+++ Scripts/UI/MainMenu/LevelThumbnail.gd
@@ -2,6 +2,7 @@ class_name UILevelTh extends Button
 
 @onready var completed = get_node("Completado")
 @onready var movements = get_node("Movimientos")
+@onready var levelNumber = get_node("Nivel")
 
 func _pressed():
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
@@ -9,8 +10,8 @@ func _pressed():
 func setData(progress):
 	var data = progress
 	movements.text = "Movimientos: " + str(data.movements)
+	levelNumber.text = "Nivel\n " + str(data.levelNumber)
 		
 	completed.visible = progress.size() > 0
 	movements.visible = progress.size() > 0
 		
-	
