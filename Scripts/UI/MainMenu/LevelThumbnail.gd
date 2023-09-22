cd70023f5d2dceddb2c97274efa5c2ac0b2cc89f
matiasbian <matiasezequielbian@gmail.com>
2023-09-22 14:18:25 -0300

    Level selector

--- Scripts/UI/MainMenu/LevelThumbnail.gd
+++ Scripts/UI/MainMenu/LevelThumbnail.gd
@@ -3,8 +3,10 @@ class_name UILevelTh extends Button
 @onready var completed = get_node("Completado")
 @onready var movements = get_node("Movimientos")
 @onready var levelNumber = get_node("Nivel")
+var levelToLoad = 1
 
 func _pressed():
+	get_node("/root/GlobalVar").level = levelToLoad
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 	
 func setData(progress):
@@ -17,5 +19,6 @@ func setData(progress):
 	movements.visible = true
 	
 func setLevel(data):
+	levelToLoad = data.level
 	levelNumber.text = "Nivel\n " + str(data.level)
 		
